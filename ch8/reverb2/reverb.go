0492bdddfd0f48b781856729ff0fcc7f11de2194
dlbas <daniel.baskal@gmail.com>
2021-04-04 10:58:26 +0300

    ch8/reverb2
    Add reverb idle timeout

--- ch8/reverb2/reverb.go
+++ ch8/reverb2/reverb.go
@@ -15,6 +15,10 @@ import (
 	"time"
 )
 
+const (
+	idleSeconds = 10 * time.Second
+)
+
 func echo(c net.Conn, shout string, delay time.Duration) {
 	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
 	time.Sleep(delay)
@@ -23,14 +27,49 @@ func echo(c net.Conn, shout string, delay time.Duration) {
 	fmt.Fprintln(c, "\t", strings.ToLower(shout))
 }
 
+func scanInput(scanner *bufio.Scanner) chan string {
+	ch := make(chan string)
+
+	go func() {
+		for scanner.Scan() { ch <- scanner.Text() }
+	}()
+
+	return ch
+}
+
+func closeConnection(c net.Conn) {
+	fmt.Fprintf(c, "closing connection after timeout %s\n", idleSeconds)
+	c.Close() // ignoring error
+}
+
+func stopAbort(timer *time.Timer) {
+	if !timer.Stop() {
+		select {
+		case <- timer.C:
+		default:
+		}
+	}
+}
+
 //!+
 func handleConn(c net.Conn) {
 	input := bufio.NewScanner(c)
-	for input.Scan() {
-		go echo(c, input.Text(), 1*time.Second)
+
+	text := scanInput(input)
+	abort := time.NewTimer(idleSeconds)
+
+	for {
+		select {
+		case s := <- text:
+			go echo(c, s, 1 * time.Second)
+			stopAbort(abort) // read abort.Reset() docs on why we should stop the timer before .Reset()
+			abort.Reset(idleSeconds)
+		case <- abort.C:
+			closeConnection(c)
+			stopAbort(abort)
+			return
+		}
 	}
-	// NOTE: ignoring potential errors from input.Err()
-	c.Close()
 }
 
 //!-
